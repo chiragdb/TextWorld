c4606596e9a36c16e62dd58c74888671c3896595
Chirag Bhatia <chiragdevbhatia@gmail.com>
2019-04-15 16:18:29 -0700

    more done

---
+++ src/Popstar.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Popstar extends Creature{
+    private Player player;
+
+    public Popstar (String name, String information, Level.Room currentRoom, Player player){
+        super(name, information, currentRoom);
+        this.player = player;
+    }
+
+    public void move(){
+        ArrayList<Level.Room> neighbors = new ArrayList<>(currentRoom.getNeighborsHash().values());
+        int listSize = neighbors.size();
+        if (listSize < 1){
+            System.out.println("No neighboring rooms exist");
+            return;
+        }
+        Level.Room playerRoom = player.getCurrentRoom();
+        ArrayList<Level.Room> nearPlayer = playerRoom.getNeighborsList();
+        ArrayList<Level.Room> possibleRooms = possibleMoves(nearPlayer, neighbors);
+
+        int possibleRoomsSize = possibleRooms.size();
+        int dNeighborsSize = neighbors.size();
+        int r = (int)(Math.random() * possibleRoomsSize);
+        Level.Room room = possibleRooms.get(r);
+        setCurrentRoom(room);
+
+
+    }
+
+    private ArrayList<Level.Room> possibleMoves(ArrayList<Level.Room> nearPlayer, ArrayList<Level.Room> neighbors) {
+        ArrayList<Level.Room> newNeighbors = neighbors;
+        for (int a = 0; a < nearPlayer.size(); a++){
+            for (int b = 0; b < neighbors.size(); b++){
+                Level.Room p = nearPlayer.get(a);
+                Level.Room pop = neighbors.get(b);
+                if (p.equals(pop)){
+                    newNeighbors.remove(pop);
+                }
+            }
+        }
+
+        return newNeighbors;
+    }
+
+}
